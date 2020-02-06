53f494edd7ee90ac317f4ec50697e9aac05a6ec1
Stewart Melville <stewart.melville@framecreates.co.uk>
2020-02-06 16:44:28 +0000

    Starting on indexing of anims

--- 7hFlevel/Indexing/FieldModels.cs
+++ 7hFlevel/Indexing/FieldModels.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _7hFlevel.FieldDictionaries;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,42 +11,20 @@ namespace _7hFlevel.Indexing
     {
         public static string RandomSwap()
         {
-            // Change over to List<string>, then I can apply randomisation.
-            // Dynamic is more for if you don't know what the data is going to be coming in
+            var typeIAnimIndex = TypeISkeleton.AssignAnims();
+            var typeIIAnimIndex = TypeIISkeleton.AssignAnims();
+            var typeOAnimIndex = TypeOSkeleton.AssignAnims();
+            var uniqueAnimIndex = UniqueSkeleton.AssignAnims();
 
-            // Dyyyyyyyyyyynamic dictionaryyyyyyyy
-            // We can set anim types to have a certain value as well.
-            // 'i' = Idle, 'w' = Walk, 'r' = Run, 'a' = Animate and so on
-            // d is reserved for dynamic object naming
+            // Convert array to a Lookup
+            var lookup = typeIAnimIndex.ToLookup(k => k.Key, v => v.Value);
 
-            dynamic d1 = new System.Dynamic.ExpandoObject(); // Dynamic object
-            dynamic d2 = new System.Dynamic.ExpandoObject(); // Dynamic object
-            dynamic d3 = new System.Dynamic.ExpandoObject(); // Dynamic object
-
-            var dict = new Dictionary<string, dynamic>();
-            dict["AAAA"] = d1; // Assign a dynamic object to the Dictionary Key
-            dict["AAGB"] = d2; // Assign a dynamic object to the Dictionary Key
-            dict["ABDA"] = d3; // Assign a dynamic object to the Dictionary Key
-
-            dict["AAAA"].FooBar = new { i1 = "ACFE", i2 = "AAFE", i3 = "CAED", i4 = "HSGB" }; // Populate the dynamic object with the value(s)
-            dict["AAGB"].FooBar = new { i1 = "ZZZZ", i2 = "XXXX", i3 = "CAED", i4 = "HSGB" }; // Populate the dynamic object with the value(s)
-            dict["ABDA"].FooBar = new { i1 = "YYYY", i2 = "WWWW", i3 = "CAED", i4 = "HSGB" }; // Populate the dynamic object with the value(s)
-
-            // Can retrieve like this: dict["AAAA"].FooBar.i1;
-            //string newName = dict["ABDA"].FooBar.i1;
-
-            string[] random = new string[] { "i1", "i2", "i3", "i4"};
-            Random rnd = new Random();
-
-            string newName = dict["ABDA"].FooBar;
-            return newName;
-
-            // Randomly allocates a Model ID
-            //Random rnd = new Random();
-            string[] nameArray = { "AAAA", "AAGB", "ABDA", "ABJB", "ACGD", "ADDA", "AEBC", "AEHD", "AFEC", "AFIE", "AGGB", "AHDF", "AIBA", "AIHB", "AJIF", };
-            int x = rnd.Next(nameArray.Length);
-            string pick = nameArray[x];
-            return pick;
+            // Retrieve random string from entry "AAAA"
+            var entry = lookup["AAAA"];
+            var rand = new Random();
+            var max = entry.Count();
+            var ans = entry.Skip(rand.Next(max)).First();        
+            return ans;
         }
 
         public static string ConsistentRandomSwap()
@@ -56,7 +35,7 @@ namespace _7hFlevel.Indexing
             return "TEST";
         }
 
-            public static string RandomAnimSwap()
+        public static string RandomAnimSwap()
         {
             // Randomly allocates an anim
             Random rnd = new Random();
